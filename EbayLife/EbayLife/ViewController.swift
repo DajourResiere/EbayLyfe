15936f8706c616b207ddc534cff86169ef5a79e2
amshen <amshen@ebay.com>
2017-07-14 23:41:15 -0700

    UI changes

--- EbayLife/EbayLife/ViewController.swift
+++ EbayLife/EbayLife/ViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    var backgroundImage : UIImageView?
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
